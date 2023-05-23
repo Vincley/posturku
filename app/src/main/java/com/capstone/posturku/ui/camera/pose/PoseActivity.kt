05179abd680c30cddfaba0490d7badcb8a00ed26
Nur Mahin <nurmahin23@gmail.com>
2023-05-24 06:15:46 +0700

    Adding activity_pose.xml

---
+++ app/src/main/java/com/capstone/posturku/ui/camera/pose/PoseActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.posturku.ui.camera.pose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.posturku.R
+
+class PoseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pose)
+    }
+}
